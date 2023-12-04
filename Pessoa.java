7b80f3c281fefac7d092838c3ae8b14b0f3c4999
Pedro <138155992+Pedroeg1234@users.noreply.github.com>
2023-12-04 19:25:32 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,25 @@
+public class Pessoa {
+
+    private String nome;
+    private int matricula;
+
+    // Getter para o campo 'nome'
+    public String getNome() {
+        return nome;
+    }
+
+    // Setter para o campo 'nome'
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Getter para o campo 'matricula'
+    public int getMatricula() {
+        return matricula;
+    }
+
+    // Setter para o campo 'matricula'
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+}
